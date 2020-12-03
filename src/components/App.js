adfba08bb7fc3e9c2864a9ea4e8f42531a0fc5ea
vimal201092 <vimalpal201092@gmail.com>
2020-12-03 18:52:56 +0530

    final

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
+import ListItem from './Listitem.js';
 import "./../styles/App.css";
 
-function App() 
-{
+function App() {
+const [items, setItems] = useState([]);
+const [newItem, setNewItem] = useState("");
+const addItem = () => {
+	items.push(newItem);
+	setItems([...items]);
+    setNewItem("");
+}
+const newItemChanged = (evt) => {
+	setNewItem(evt.target.value);
+}
+const editHandler = (editedValue,itemIdx) => {
+	items[itemIdx] = editedValue;
+	setItems([...items])
+  }
+const deleteHandler = (itemIdx) => {
+  items.splice(itemIdx, 1);
+  setItems([...items])
+}
 	return (
-	<div id="main">
-	//Do not alter main div
-	//Please do not alter the functional component as tests depend on the type of component.
-	</div>
+	 <div id="main">
+	<textarea id="task" onChange={newItemChanged} placeholder="New Item" value={newItem}></textarea>
+	<button id="btn" onClick={addItem} 
+	disabled={newItem.trim().length === 0}>Add Item</button>
+	{items.map((item ,idx) => (
+		<ListItem item={item} 
+		key={`${item}_${idx}`} 
+		idx={idx} 
+		editHandler={editHandler} 
+		deleteHandler={deleteHandler}/>
+		))}
+	 </div>
 	);
+
 }
 
 
